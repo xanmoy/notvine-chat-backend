018a912a5bc2b2da9ee0916feb32477e02af5b63
Xanmoy <xanmoy@outlook.com>
2023-10-29 03:25:25 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+
+const authRoutes = require('./routes/auth.js');
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get('/', (req, res) => {
+    res.send('Hello to Notvine API Powered by Xanmoy');
+});
+
+app.use('/auth', authRoutes);
+
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
